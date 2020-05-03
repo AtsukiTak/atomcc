cbe4cf958c78548e0aca93022da336eb0a395c2f
AtsukiTak <takatomgoo@gmail.com>
2020-05-03 14:43:43 +0900

    引数に受け取った数字を終了コードにするアセンブリの出力

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let i = {
+        let s = std::env::args().nth(1).unwrap();
+        usize::from_str_radix(s.as_str(), 10).unwrap()
+    };
+
+    println!(".intel_syntax noprefix");
+    println!(".global _main");
+    println!("_main:");
+    println!("  mov rax, {}", i);
+    println!("  ret");
 }
