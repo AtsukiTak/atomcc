9f994214110f7e2ad828d3d84a13801eb2e857a0
AtsukiTak <takatomgoo@gmail.com>
2020-05-07 20:16:18 +0900

    TokenIterにn_bytesフィールドを追加

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -7,10 +7,12 @@ pub enum Token {
 
 pub struct TokenIter<'a> {
     s: &'a str,
+    // 現在の文字が全体の何文字目か
+    n_bytes: usize,
 }
 
 pub fn tokenize<'a>(s: &'a str) -> TokenIter<'a> {
-    TokenIter { s }
+    TokenIter { s, n_bytes: 0 }
 }
 
 impl Token {
@@ -26,28 +28,35 @@ impl<'a> Iterator for TokenIter<'a> {
     type Item = Token;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.s = self.s.trim_start();
+        self.update_s(self.s.trim_start());
         if self.s.is_empty() {
             return None;
         }
 
         if self.s.as_bytes()[0] == b'+' {
-            self.s = self.s.split_at(1).1;
+            self.update_s(self.s.split_at(1).1);
             return Some(Token::Plus);
         }
 
         if self.s.as_bytes()[0] == b'-' {
-            self.s = self.s.split_at(1).1;
+            self.update_s(self.s.split_at(1).1);
             return Some(Token::Minus);
         }
 
         let (digit_s, remain_s) = split_digit(self.s);
         if !digit_s.is_empty() {
-            self.s = remain_s;
+            self.update_s(remain_s);
             return Some(Token::Num(usize::from_str_radix(digit_s, 10).unwrap()));
         }
 
         panic!("Invalid token stream")
+    }
+}
+
+impl<'a> TokenIter<'a> {
+    fn update_s(&mut self, new_s: &'a str) {
+        self.n_bytes += self.s.len() - new_s.len();
+        self.s = new_s;
     }
 }
 
