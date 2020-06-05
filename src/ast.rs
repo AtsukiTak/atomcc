9875c672109950f432511f7af1d4586b8a73c3d0
AtsukiTak <takatomgoo@gmail.com>
2020-06-05 15:45:54 +0900

    ASTパーサをast::Parserのメソッドに移行

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,5 @@
 use crate::token::{Op, Par, Token, TokenIter, TokenKind};
+use std::collections::HashMap;
 
 pub enum Node<'a> {
     Assign(AssignNode<'a>),
@@ -45,197 +46,225 @@ impl<'a> ExprNode<'a> {
     }
 }
 
-/// > program       = stmt*
-/// > stmt          = assign ";"
-/// > assign        = (ident "=")? expr
-/// > expr          = equality
-/// > equality      = relational ("==" relational | "!=" relational)*
-/// > relational    = add ("<" add | "<=" add | ">" add | ">=" add)*
-/// > add           = mul ("+" mul | "-" mul)*
-/// > unary         = ("+" | "-")? primary
-/// > primary       = num | ident | "(" expr ")"
-///
-/// で表現される文法をパースする関数。
-pub fn parse<'a>(tokens: &mut TokenIter<'a>) -> Vec<Node<'a>> {
-    program(tokens)
+pub struct Parser<'a> {
+    local_vars: HashMap<&'a str, usize>,
 }
 
-/// > program       = stmt*
-pub fn program<'a>(tokens: &mut TokenIter<'a>) -> Vec<Node<'a>> {
-    let mut nodes = Vec::new();
-    while let Some(_) = tokens.peek() {
-        nodes.push(stmt(tokens))
+impl<'a> Parser<'a> {
+    pub fn new() -> Self {
+        Parser {
+            local_vars: HashMap::new(),
+        }
     }
-    nodes
-}
 
-/// > stmt          = assign ";"
-pub fn stmt<'a>(tokens: &mut TokenIter<'a>) -> Node<'a> {
-    let node = assign(tokens);
-    match tokens.next() {
-        Some(Token {
-            kind: TokenKind::Semi,
-            ..
-        }) => node,
-        Some(t) => t.exit_with_err_msg("expected \";\" but found another"),
-        None => tokens.exit_with_err_msg("expected \";\" but found EOF"),
+    /// > program       = stmt*
+    /// > stmt          = assign ";"
+    /// > assign        = (ident "=")? expr
+    /// > expr          = equality
+    /// > equality      = relational ("==" relational | "!=" relational)*
+    /// > relational    = add ("<" add | "<=" add | ">" add | ">=" add)*
+    /// > add           = mul ("+" mul | "-" mul)*
+    /// > unary         = ("+" | "-")? primary
+    /// > primary       = num | ident | "(" expr ")"
+    ///
+    /// で表現される文法をパースする関数。
+    pub fn parse(&mut self, tokens: &mut TokenIter<'a>) -> Vec<Node<'a>> {
+        self.parse_program(tokens)
     }
-}
 
-/// > assign        = (ident "=")? expr
-pub fn assign<'a>(tokens: &mut TokenIter<'a>) -> Node<'a> {
-    // 与えられたTokenIterが (ident "=") で始まるかチェックする
-    let mut tokens2 = *tokens;
-    match (tokens2.next(), tokens2.next()) {
-        // (ident "=") で始まった場合のルート.
-        // tokensを2つ進める。
-        (
+    /// > program       = stmt*
+    ///
+    /// で表現される記号programをパースする関数。
+    pub fn parse_program(&mut self, tokens: &mut TokenIter<'a>) -> Vec<Node<'a>> {
+        let mut nodes = Vec::new();
+        while let Some(_) = tokens.peek() {
+            nodes.push(self.parse_stmt(tokens))
+        }
+        nodes
+    }
+
+    /// > stmt          = assign ";"
+    ///
+    /// で表現される記号stmtをパースする関数。
+    pub fn parse_stmt(&mut self, tokens: &mut TokenIter<'a>) -> Node<'a> {
+        let node = self.parse_assign(tokens);
+        match tokens.next() {
             Some(Token {
-                kind: TokenKind::Ident(s),
+                kind: TokenKind::Semi,
                 ..
-            }),
-            Some(Token {
-                kind: TokenKind::Op(Op::Assign),
-                ..
-            }),
-        ) => {
-            tokens.next();
-            tokens.next();
-            Node::Assign(AssignNode {
-                lhs_ident: s,
-                rhs: expr(tokens),
-            })
-        }
-        // (ident "=") で始まらなかった場合のルート.
-        // tokensは進んでいないことに注意。
-        _ => Node::Expr(expr(tokens)),
-    }
-}
-
-/// > expr          = equality
-pub fn expr<'a>(tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
-    equality(tokens)
-}
-
-/// > equality      = relational ("==" relational | "!=" relational)*
-pub fn equality<'a>(tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
-    let mut node = relational(tokens);
-    while let Some(token) = tokens.peek() {
-        let op = match token.op() {
-            Some(op @ Op::Eq) => op,
-            Some(op @ Op::Neq) => op,
-            _ => break,
-        };
-
-        // このルートに入ることが確定したのでイテレータを進める
-        let _ = tokens.next();
-        let rhs = relational(tokens);
-        node = ExprNode::new_op(op, node, rhs);
-    }
-    node
-}
-
-/// > relational    = ("<" add | "<=" add | ">" add | ">=" add)*
-pub fn relational<'a>(tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
-    let mut node = add(tokens);
-    while let Some(token) = tokens.peek() {
-        let (op, reverse) = match token.op() {
-            Some(op @ Op::Lt) => (op, false),
-            Some(op @ Op::Lte) => (op, false),
-            Some(Op::Gt) => (Op::Lt, true),
-            Some(Op::Gte) => (Op::Lte, true),
-            _ => break,
-        };
-
-        // このルートに入ることが確定したのでイテレータを進める
-        let _ = tokens.next();
-        if reverse {
-            node = ExprNode::new_op(op, add(tokens), node);
-        } else {
-            node = ExprNode::new_op(op, node, add(tokens));
+            }) => node,
+            Some(t) => t.exit_with_err_msg("expected \";\" but found another"),
+            None => tokens.exit_with_err_msg("expected \";\" but found EOF"),
         }
     }
-    node
-}
 
-/// > add           = mul ("+" mul | "-" mul)*
-pub fn add<'a>(tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
-    let mut node = mul(tokens);
-    while let Some(token) = tokens.peek() {
-        let op = match token.op() {
-            Some(op @ Op::Add) => op,
-            Some(op @ Op::Sub) => op,
-            _ => break,
-        };
-
-        // このルートに入ることが確定したのでイテレータを進める
-        let _ = tokens.next();
-        let rhs = mul(tokens);
-        node = ExprNode::new_op(op, node, rhs);
+    /// > assign        = (ident "=")? expr
+    ///
+    /// で表現される記号assignをパースする関数。
+    pub fn parse_assign(&mut self, tokens: &mut TokenIter<'a>) -> Node<'a> {
+        // 与えられたTokenIterが (ident "=") で始まるかチェックする
+        let mut tokens2 = *tokens;
+        match (tokens2.next(), tokens2.next()) {
+            (
+                Some(Token {
+                    kind: TokenKind::Ident(s),
+                    ..
+                }),
+                Some(Token {
+                    kind: TokenKind::Op(Op::Assign),
+                    ..
+                }),
+            ) => {
+                // (ident "=") で始まった場合のルート.
+                // tokensを2つ進める。
+                tokens.next();
+                tokens.next();
+                Node::Assign(AssignNode {
+                    lhs_ident: s,
+                    rhs: self.parse_expr(tokens),
+                })
+            }
+            // (ident "=") で始まらなかった場合のルート.
+            // tokensは進んでいないことに注意。
+            _ => Node::Expr(self.parse_expr(tokens)),
+        }
     }
-    node
-}
 
-/// > mul       = unary ("*" unary | "/" unary)*
-pub fn mul<'a>(tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
-    let mut node = unary(tokens);
-    while let Some(token) = tokens.peek() {
-        let op = match token.op() {
-            Some(op @ Op::Mul) => op,
-            Some(op @ Op::Div) => op,
-            _ => break,
-        };
-
-        // このルートに入ることが確定したのでイテレータを進める
-        let _ = tokens.next();
-        node = ExprNode::new_op(op, node, unary(tokens));
+    /// > expr          = equality
+    ///
+    /// で表現される記号exprをパースする関数。
+    pub fn parse_expr(&mut self, tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
+        self.parse_equality(tokens)
     }
-    node
-}
 
-/// > unary     = ("+" | "-")? primary
-///
-/// で表現される非終端記号unaryをパースする関数。
-pub fn unary<'a>(tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
-    match tokens.peek().and_then(|token| token.op()) {
-        Some(Op::Add) => {
+    /// > equality      = relational ("==" relational | "!=" relational)*
+    ///
+    /// で表現される記号equalityをパースする関数。
+    pub fn parse_equality(&mut self, tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
+        let mut node = self.parse_relational(tokens);
+        while let Some(token) = tokens.peek() {
+            let op = match token.op() {
+                Some(op @ Op::Eq) => op,
+                Some(op @ Op::Neq) => op,
+                _ => break,
+            };
+
+            // このルートに入ることが確定したのでイテレータを進める
             let _ = tokens.next();
-            ExprNode::new_op(Op::Add, ExprNode::new_num(0), primary(tokens))
+            let rhs = self.parse_relational(tokens);
+            node = ExprNode::new_op(op, node, rhs);
         }
-        Some(Op::Sub) => {
-            let _ = tokens.next();
-            ExprNode::new_op(Op::Sub, ExprNode::new_num(0), primary(tokens))
-        }
-        _ => primary(tokens),
-    }
-}
-
-/// > primary   = num | ident | "(" expr ")"
-///
-/// で表現される非終端記号primaryをパースする関数。
-pub fn primary<'a>(tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
-    let token = tokens.next().unwrap_or_else(|| {
-        tokens.exit_with_err_msg("Unexpected EOF. number, ident or \"(\" is expected")
-    });
-
-    if let Some(n) = token.num() {
-        ExprNode::new_num(n)
-    } else if let Some(c) = token.ident() {
-        ExprNode::new_ident(c)
-    } else {
-        if !matches!(token.expect_par(), Par::Left) {
-            token.exit_with_err_msg("expect \"(\" instead of \")\"");
-        }
-
-        let node = expr(tokens);
-
-        let token = tokens
-            .next()
-            .unwrap_or_else(|| tokens.exit_with_err_msg("Unexpected EOF. \")\" is expected"));
-        if !matches!(token.expect_par(), Par::Right) {
-            token.exit_with_err_msg("expect \")\" instead of \"(\"");
-        }
-
         node
+    }
+
+    /// > relational    = ("<" add | "<=" add | ">" add | ">=" add)*
+    ///
+    /// で表現される記号relationalをパースする関数。
+    pub fn parse_relational(&mut self, tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
+        let mut node = self.parse_add(tokens);
+        while let Some(token) = tokens.peek() {
+            let (op, reverse) = match token.op() {
+                Some(op @ Op::Lt) => (op, false),
+                Some(op @ Op::Lte) => (op, false),
+                Some(Op::Gt) => (Op::Lt, true),
+                Some(Op::Gte) => (Op::Lte, true),
+                _ => break,
+            };
+
+            // このルートに入ることが確定したのでイテレータを進める
+            let _ = tokens.next();
+            if reverse {
+                node = ExprNode::new_op(op, self.parse_add(tokens), node);
+            } else {
+                node = ExprNode::new_op(op, node, self.parse_add(tokens));
+            }
+        }
+        node
+    }
+
+    /// > add           = mul ("+" mul | "-" mul)*
+    ///
+    /// で表現される記号addをパースする関数。
+    pub fn parse_add(&mut self, tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
+        let mut node = self.parse_mul(tokens);
+        while let Some(token) = tokens.peek() {
+            let op = match token.op() {
+                Some(op @ Op::Add) => op,
+                Some(op @ Op::Sub) => op,
+                _ => break,
+            };
+
+            // このルートに入ることが確定したのでイテレータを進める
+            let _ = tokens.next();
+            let rhs = self.parse_mul(tokens);
+            node = ExprNode::new_op(op, node, rhs);
+        }
+        node
+    }
+
+    /// > mul       = unary ("*" unary | "/" unary)*
+    ///
+    /// で表現される記号mulをパースする関数。
+    pub fn parse_mul(&mut self, tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
+        let mut node = self.parse_unary(tokens);
+        while let Some(token) = tokens.peek() {
+            let op = match token.op() {
+                Some(op @ Op::Mul) => op,
+                Some(op @ Op::Div) => op,
+                _ => break,
+            };
+
+            // このルートに入ることが確定したのでイテレータを進める
+            let _ = tokens.next();
+            node = ExprNode::new_op(op, node, self.parse_unary(tokens));
+        }
+        node
+    }
+
+    /// > unary     = ("+" | "-")? primary
+    ///
+    /// で表現される記号unaryをパースする関数。
+    pub fn parse_unary(&mut self, tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
+        match tokens.peek().and_then(|token| token.op()) {
+            Some(Op::Add) => {
+                let _ = tokens.next();
+                ExprNode::new_op(Op::Add, ExprNode::new_num(0), self.parse_primary(tokens))
+            }
+            Some(Op::Sub) => {
+                let _ = tokens.next();
+                ExprNode::new_op(Op::Sub, ExprNode::new_num(0), self.parse_primary(tokens))
+            }
+            _ => self.parse_primary(tokens),
+        }
+    }
+
+    /// > primary   = num | ident | "(" expr ")"
+    ///
+    /// で表現される記号primaryをパースする関数。
+    pub fn parse_primary(&mut self, tokens: &mut TokenIter<'a>) -> ExprNode<'a> {
+        let token = tokens.next().unwrap_or_else(|| {
+            tokens.exit_with_err_msg("Unexpected EOF. number, ident or \"(\" is expected")
+        });
+
+        if let Some(n) = token.num() {
+            ExprNode::new_num(n)
+        } else if let Some(c) = token.ident() {
+            ExprNode::new_ident(c)
+        } else {
+            if !matches!(token.expect_par(), Par::Left) {
+                token.exit_with_err_msg("expect \"(\" instead of \")\"");
+            }
+
+            let node = self.parse_expr(tokens);
+
+            let token = tokens
+                .next()
+                .unwrap_or_else(|| tokens.exit_with_err_msg("Unexpected EOF. \")\" is expected"));
+            if !matches!(token.expect_par(), Par::Right) {
+                token.exit_with_err_msg("expect \")\" instead of \"(\"");
+            }
+
+            node
+        }
     }
 }
