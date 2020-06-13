4c78d67a5c10c1d87cca09176c928a307dc8808c
AtsukiTak <takatomgoo@gmail.com>
2020-06-13 14:06:38 +0900

    add Sub type

--- src/asm/op/mod.rs
+++ src/asm/op/mod.rs
@@ -1,5 +1,7 @@
 mod mov;
 mod push;
+mod sub;
 
 pub use mov::Mov;
 pub use push::Push;
+pub use sub::Sub;
--- src/asm/op/sub.rs
+++ src/asm/op/sub.rs
@@ -1,22 +1,22 @@
 use super::super::{Instruction, Reg64};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Sub<S, D> {
+pub struct Sub<D, S> {
     dst: D,
     src: S,
 }
 
-impl Mov<Reg64, Reg64> {
-    pub fn new(dst: Reg64, src: Reg64) -> Self {
-        Mov { dst, src }
+impl Sub<Reg64, i64> {
+    pub fn new(dst: Reg64, src: i64) -> Self {
+        Sub { dst, src }
     }
 }
 
-impl Instruction for Mov<Reg64, Reg64> {
+impl Instruction for Sub<Reg64, i64> {
     fn write<W>(&self, w: &mut W) -> std::io::Result<()>
     where
         W: std::io::Write,
     {
-        write!(w, "  mov {}, {}\n", self.dst, self.src)
+        write!(w, "  sub {}, {}\n", self.dst, self.src)
     }
 }
