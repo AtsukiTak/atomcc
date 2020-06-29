c81d0e1aa8dd476714905ae4a40ad5cbbb8c8487
AtsukiTak <takatomgoo@gmail.com>
2020-06-29 15:16:29 +0900

    Stmtの修正

--- src/parser/node.rs
+++ src/parser/node.rs
@@ -1,6 +1,7 @@
 use super::op::BinOp;
 use crate::token::token::*;
 
+#[derive(Debug, Clone)]
 pub enum Stmt<'src> {
     Assign(StmtAssign<'src>),
     Block(StmtBlock<'src>),
@@ -10,6 +11,7 @@ pub enum Stmt<'src> {
     While(StmtWhile<'src>),
 }
 
+#[derive(Debug, Clone)]
 pub enum Expr<'src> {
     Num(Num<'src>),
     Ident(ExprIdent<'src>),
@@ -18,15 +20,17 @@ pub enum Expr<'src> {
 }
 
 /// "hoge = 42;"
+#[derive(Debug, Clone)]
 pub struct StmtAssign<'src> {
     pub lhs_offset: usize,
     pub lhs: Ident<'src>,
     pub assign_token: Assign<'src>,
     pub rhs: Expr<'src>,
-    pub semi: Semi<'src>,
+    pub semi_token: Semi<'src>,
 }
 
 /// "{ hoge = 42; return hoge; }"
+#[derive(Debug, Clone)]
 pub struct StmtBlock<'src> {
     pub brace_left_token: BraceLeft<'src>,
     pub stmts: Vec<Stmt<'src>>,
@@ -34,14 +38,16 @@ pub struct StmtBlock<'src> {
 }
 
 /// "return 42;"
+#[derive(Debug, Clone)]
 pub struct StmtReturn<'src> {
     pub return_token: Return<'src>,
     pub expr: Expr<'src>,
-    pub semi: Semi<'src>,
+    pub semi_token: Semi<'src>,
 }
 
 /// "if (true) { 42 }"
 /// "if (i = 0) { 42 } else { 24 }"
+#[derive(Debug, Clone)]
 pub struct StmtIf<'src> {
     pub if_token: If<'src>,
     pub paren_left_token: ParenLeft<'src>,
@@ -52,21 +58,26 @@ pub struct StmtIf<'src> {
 }
 
 /// "while (i < 10) { i = i + 1 }"
+#[derive(Debug, Clone)]
 pub struct StmtWhile<'src> {
     pub while_token: While<'src>,
     pub paren_left_token: ParenLeft<'src>,
     pub cond: Expr<'src>,
     pub paren_right_token: ParenRight<'src>,
-    pub block: StmtBlock<'src>,
+    // TODO
+    // StmtBlockになるようにパーサを修正する
+    pub block: Box<Stmt<'src>>,
 }
 
 /// "hoge"
+#[derive(Debug, Clone)]
 pub struct ExprIdent<'src> {
     pub ident_offset: usize,
     pub ident: Ident<'src>,
 }
 
 /// "func()"
+#[derive(Debug, Clone)]
 pub struct ExprCall<'src> {
     pub ident: Ident<'src>,
     pub paren_left_token: ParenLeft<'src>,
@@ -74,6 +85,7 @@ pub struct ExprCall<'src> {
 }
 
 /// "4 * 2"
+#[derive(Debug, Clone)]
 pub struct ExprBinOp<'src> {
     pub lhs: Box<Expr<'src>>,
     pub op: BinOp<'src>,
