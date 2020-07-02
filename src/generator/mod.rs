f7c869e61c014f3560874f4128928ecf0f69a335
AtsukiTak <takatomgoo@gmail.com>
2020-07-02 19:40:20 +0900

    get_unique_num 関数の追加

--- src/generator/mod.rs
+++ src/generator/mod.rs
@@ -2,3 +2,13 @@ mod generator;
 mod subroutine;
 
 pub use generator::Generator;
+
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+static COUNTER: AtomicUsize = AtomicUsize::new(0);
+
+/// GlobalにUniqueな数値を取得する。
+/// GlobalにUniqueなラベルを生成するのに使用する
+pub fn get_unique_num() -> usize {
+    COUNTER.fetch_add(1, Ordering::SeqCst)
+}
